fc5ee3d0726fb37e23ad567dcf2b492d8770e4c9
smildiner <smildiner@gmail.com>
2020-04-22 10:37:09 +0200

    added run_one_sim() function and documentation

---
+++ R/run_one_sim.R
@@ -0,0 +1,23 @@
+#' Run one simulation
+#'
+#' @importFrom dplyr select
+#' @importFrom magrittr %>%
+#' @importFrom purrr map
+#'
+#' @export
+
+run_one_sim <- function(uid, seed){
+
+    # Get simulation parameters
+
+    # Simulate data
+
+    # Fit mHMMbayes model
+
+    # Get MAP estimates
+
+    # Get evaluation metrics
+
+    # Save results
+
+}
