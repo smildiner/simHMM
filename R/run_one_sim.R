7fd5fc9c7a1c65a9d525bb40793fb44e60dba108
smildiner <smildiner@gmail.com>
2020-04-28 10:39:20 +0200

    added cci_out call

--- R/run_one_sim.R
+++ R/run_one_sim.R
@@ -71,12 +71,14 @@ run_one_sim <- function(uid, seed){
     # Get MAP estimates
     map_out <- MAP(model_output)
 
-    # Add info
+    # Get credibility intervals
+    cci_out <- get_cci(model_output)
+
+    # Add scenario and iteration info
     map_out <- c(scenario_uid = model_pars[["scenario_uid"]],
                  uid = model_pars[["uid"]],
-                 map_out)
-
-    # Add credibility intervals
+                 map_out,
+                 cci_out)
 
     # Get evaluation metrics: not for now
 
